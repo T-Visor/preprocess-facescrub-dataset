8597bdb1ce4c63cbd2fd3debc68854bfd8e95820
Turhan Akira <turkimbro@yahoo.com>
2022-05-31 23:48:39 -0400

    added commands for fawkes and lowkey datasets

--- commands.sh
+++ commands.sh
@@ -5,3 +5,9 @@ for directory in ./*/ ; do (cd '$directory' && mogrify -format jpg *.jpeg); done
 
 # Remove the old '.jpeg' files.
 for directory in ./*/ ; do (cd '$directory' && rm *.jpeg); done
+
+# Remove redundant files from Lowkey preprocessed dataset.
+find . -type f ! -name '*_attacked.png' -delete
+
+# Remove redundant files from Fawkes preprocessed dataset.
+find . -type f ! -name '*_cloaked.png' -delete
